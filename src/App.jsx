fba71b349f9e9e1becd22a46e6c6cf89dda78e40
wjdguswhd <guswhd43@naver.com>
2026-06-10 19:43:28 +0900

    fix:board관련 route추가 및 import정리

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import Login from './layouts/Login';
 import Signup from './layouts/Signup';
 import Main from './layouts/Main';
 import EditProfile from './layouts/EditProfile';
-
-import BoardFree from './layouts/BoardFree';
-import BoardFreeDetail from './layouts/BoardFreeDetail';
-
 import MyPage from './layouts/MyPage';
 import WelfareList from './layouts/WelfareList';
 import WelfareDetail from './layouts/WelfareDetail';
 import Persona from './layouts/Persona';
+import BoardFree from './layouts/BoardFree';
+import BoardFreeDetail from './layouts/BoardFreeDetail';
+import BoardFreeWrite from './layouts/BoardFreeWrite';
+import NoticeBoard from './layouts/NoticeBoard';
+import NoticeWrite from './layouts/NoticeWrite';
 
 function App() {
   return (
     <BrowserRouter>
-    {/* 나중에 provider로 묶고 navbar 하드코딩 수정 */}
+    {/* 나중에 provider로 묶고 navbar 하드코딩 수정,board들은 나중에id추가 */}
       <Navbar isLoggedIn={false} nickname="석희" />
       <Routes>
         <Route path="/" element={<Main />} />
@@ -28,6 +29,11 @@ function App() {
         <Route path="/welfarelist" element={<WelfareList />} />
         <Route path="/welfaredetail" element={<WelfareDetail />} />
         <Route path="/persona" element={<Persona />} />
+        <Route path="/boardfree" element={<BoardFree />} />
+        <Route path="/boardfreedetail" element={<BoardFreeDetail />} />
+        <Route path="/boardfreewrite" element={<BoardFreeWrite />} />
+        <Route path="/noticeboard" element={<NoticeBoard />} />
+        <Route path="/noticewrite" element={<NoticeWrite />} />
       </Routes>
     </BrowserRouter>
   );
